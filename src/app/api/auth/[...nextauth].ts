2c362583fedef09e59d6dbd5948368ced4f50976
Sai Bhaskar <saibhaskar.tamatapu@soham.ai>
2023-10-12 20:37:38 +0530

    added max width wrapper and next auth

---
+++ src/app/api/auth/[...nextauth].ts
@@ -0,0 +1,9 @@
+// import nextAuth from "next-auth";
+// import NextAuth from "next-auth"
+// import NextAuth from "next-auth/next"
+
+export const authOptions = {
+  providers: [],
+};
+
+// export default nextAuth(authOptions)
