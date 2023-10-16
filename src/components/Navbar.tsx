53185260c291181d5ed4c8a290857ad54eea2cfd
Sai Bhaskar <saibhaskar.tamatapu@soham.ai>
2023-10-16 16:17:28 +0530

    removed ts error comment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -65,7 +65,6 @@ const Navbar: FC<NavbarProps> = ({}) => {
                   Dashboard
                 </Link>
 
-                {/* @ts-expect-error Async Server Component */}
                 <UserAccountNav
                   name={
                     !user.given_name || !user.family_name
